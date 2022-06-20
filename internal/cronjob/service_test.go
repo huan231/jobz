bb1d206d2f252bb36ba2427dd00b5f4736c30142
huan231 <43064938+huan231@users.noreply.github.com>
2022-06-20 16:36:39 +0200

    test: cron job service (#11)

---
+++ internal/cronjob/service_test.go
@@ -0,0 +1,52 @@
+package cronjob
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	batchv1 "k8s.io/api/batch/v1"
+	"k8s.io/apimachinery/pkg/labels"
+	v1 "k8s.io/client-go/listers/batch/v1"
+	"testing"
+)
+
+func TestList(t *testing.T) {
+	l := &listerMock{t: t, cronJobs: []*batchv1.CronJob{{}}}
+
+	s := NewService(l)
+
+	cronJobs, _ := s.List()
+
+	assert.NotEmpty(t, cronJobs)
+}
+
+func TestListError(t *testing.T) {
+	errUnexpected := fmt.Errorf("unexpected error")
+
+	l := &listerMock{err: errUnexpected}
+
+	s := NewService(l)
+
+	_, err := s.List()
+
+	assert.Equal(t, errUnexpected, err)
+}
+
+type listerMock struct {
+	err      error
+	t        *testing.T
+	cronJobs []*batchv1.CronJob
+}
+
+func (l *listerMock) List(selector labels.Selector) ([]*batchv1.CronJob, error) {
+	if l.err != nil {
+		return nil, l.err
+	}
+
+	assert.Equal(l.t, labels.Everything(), selector)
+
+	return l.cronJobs, nil
+}
+
+func (l *listerMock) CronJobs(string) v1.CronJobNamespaceLister {
+	return nil
+}
